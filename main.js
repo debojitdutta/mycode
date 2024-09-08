04d69f158a7cc977a175fa478a4cbd7fe6291c47
Debojit Dutta <155905079+debojitdutta@users.noreply.github.com>
2024-09-08 17:45:49 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+var settingsmenu = document.querySelector(".profile-settings");
+// var darkbtn = document.getElementById("dark-btn");
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("profile-settings-height");
+}

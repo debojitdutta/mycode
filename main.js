5b897665f7a851f2abf1bb983b4d939ae0b1d3a0
Debojit Dutta <155905079+debojitdutta@users.noreply.github.com>
2024-11-25 21:00:31 +0530

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-var settingsmenu = document.querySelector(".profile-settings");
-// var darkbtn = document.getElementById("dark-btn");
-function settingsMenuToggle() {
-    settingsmenu.classList.toggle("profile-settings-height");
-}
